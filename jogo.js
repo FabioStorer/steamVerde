d38bb373917cc10145b278fab72911d2190860cc
Fabio.Storer <fabiostorer19@gmail.com>
2024-07-23 11:20:24 -0300

    feat: registro concluido

--- jogo.js
+++ jogo.js
@@ -15,7 +15,7 @@ const registro = () => {
 
     while (true) {
         jogo.anoLancamento = Number(prompt('Qual é o ano de lancamento do jogo? '));
-        if (jogo.anoLancamento < 1958 || jogo.anoLancamento > 2024 || isNaN(anoLancamento) ) {
+        if (jogo.anoLancamento < 1958 || jogo.anoLancamento > 2024 || isNaN(jogo.anoLancamento) ) {
             console.log('Ano inválido.');
         } else {
             break;
@@ -24,10 +24,39 @@ const registro = () => {
 
     while (true) {
         jogo.duracao = Number(prompt('Qual é a duração do jogo? ').replaceAll(',', '.'));
-        if (jogo.duracao <= 0 || jogo.duracao > 2024 || isNaN(duracao) ) {
+        if (jogo.duracao <= 0 || isNaN(jogo.duracao) ) {
             console.log('Duração inválido.');
         } else {
             break;
         }
     }
+
+    while (true) {
+        jogo.preco = Number(prompt('Qual é o preço do jogo? ').replaceAll(',', '.'));
+        if (jogo.preco != 0 || isNaN(jogo.preco) ) {
+            console.log('Preço inválido.');
+        } else {
+            break;
+        }
+    }
+
+    while (true) {
+        jogo.estudio = prompt('Qual é o estúdio do jogo? ');
+        if (jogo.estudio == '') {
+            console.log('O estúdio não pode ser vazio.');
+        } else {
+            break;
+        }
+    }
+
+    while (true) {
+        jogo.sequencia = parseInt(prompt('Qual é a sequencia do jogo? '));
+        if (jogo.sequencia < 0 || jogo.sequencia >= jogos.length) {
+            console.log('A sequencia não pode ser vazio.');
+        } else {
+            break;
+        }
+    }
+
+    jogos.push(jogo);
 };
