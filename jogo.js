dfa855e9771ad5d36e7ade3bd776d60b69ccc9f0
Fabio.Storer <fabiostorer19@gmail.com>
2024-07-23 11:36:40 -0300

    feat: criando as outras funções do crud

--- jogo.js
+++ jogo.js
@@ -1,7 +1,7 @@
 const prompt = require('prompt-sync')();
 const jogos = [];
 
-const registro = () => {
+const modelo = () => {
     let jogo
 
     while (true) {
@@ -15,7 +15,7 @@ const registro = () => {
 
     while (true) {
         jogo.anoLancamento = Number(prompt('Qual é o ano de lancamento do jogo? '));
-        if (jogo.anoLancamento < 1958 || jogo.anoLancamento > 2024 || isNaN(jogo.anoLancamento) ) {
+        if (jogo.anoLancamento < 1958 || jogo.anoLancamento > 2024 || isNaN(jogo.anoLancamento)) {
             console.log('Ano inválido.');
         } else {
             break;
@@ -24,7 +24,7 @@ const registro = () => {
 
     while (true) {
         jogo.duracao = Number(prompt('Qual é a duração do jogo? ').replaceAll(',', '.'));
-        if (jogo.duracao <= 0 || isNaN(jogo.duracao) ) {
+        if (jogo.duracao <= 0 || isNaN(jogo.duracao)) {
             console.log('Duração inválido.');
         } else {
             break;
@@ -33,7 +33,7 @@ const registro = () => {
 
     while (true) {
         jogo.preco = Number(prompt('Qual é o preço do jogo? ').replaceAll(',', '.'));
-        if (jogo.preco != 0 || isNaN(jogo.preco) ) {
+        if (jogo.preco != 0 || isNaN(jogo.preco)) {
             console.log('Preço inválido.');
         } else {
             break;
@@ -50,6 +50,10 @@ const registro = () => {
     }
 
     while (true) {
+        if (jogos.length == 0) {
+            jogos.sequencia = undefined;
+            break;
+        }
         jogo.sequencia = parseInt(prompt('Qual é a sequencia do jogo? '));
         if (jogo.sequencia < 0 || jogo.sequencia >= jogos.length) {
             console.log('A sequencia não pode ser vazio.');
@@ -57,6 +61,37 @@ const registro = () => {
             break;
         }
     }
+    return jogo;
+};
+
+const criar = () => {
+
+    let jogo = modelo();
 
     jogos.push(jogo);
+
+    console.log('Jogo cadastrado com sucesso.');
+};
+
+const lista = () => {
+    jogos.forEach((jogo, i) => {
+        console.log(`${i + 1}. ${jogo.nome}, ${jogo.anoLancamento}, ${jogo.duracao}, ${jogo.preco}, ${jogo.estudio}, ${jogo.sequencia}`);
+    })
+};
+
+const atualizar = () => {
+    while (true) {
+        if (jogos.length == 0) {
+            console.log('Nenhum jogo cadastrado.');
+            return;
+        }
+        let indice = parseInt(prompt('Qual índice do jogo que precisa atualizar?'));
+        if (indice < 0 || indice >= jogos.length || isNaN(indice)) {
+            console.log('Índice inválido.');
+        } else {
+            const jogo = modelo();
+            jogos(indice) = jogo;
+            break;
+        }
+    }
 };
